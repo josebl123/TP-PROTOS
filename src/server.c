4aac4eec832af3c0237f8b6c28e5bfa8683fa7f9
Nicolas Koron <nkoron@itba.edu.ar>
2025-06-24 21:48:04 +0000

    minor changes on server

--- src/server.c
+++ src/server.c
@@ -16,19 +16,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+#include "selector.h"
+#include "utils/server_utils.h"
 
 #define TRUE   1
 #define FALSE  0
-#define PORT 8888
+#define PORT "8888"
+#define INITIAL_MAX_CLIENTS 30
+#define MAX_BUFFER_SIZE 1024
 
 int main(int argc , char *argv[])
 {
     int opt = TRUE;
-    int master_socket , addrlen , new_socket , client_socket[30] , max_clients = 30 , activity, i , valread , sd;
+    int master_socket , addrlen , new_socket , client_socket[INITIAL_MAX_CLIENTS] , max_clients = INITIAL_MAX_CLIENTS , activity, i , valread , sd;
     int max_sd;
     struct sockaddr_in address;
 
-    char buffer[1025];  //data buffer of 1K
+    char buffer[MAX_BUFFER_SIZE];  //data buffer of 1K
 
     //set of socket descriptors
     fd_set readfds;
@@ -36,58 +40,43 @@ int main(int argc , char *argv[])
     //a message
     char *message = "ECHO Daemon v1.0 \r\n";
 
+    selector_init selector_init = {
+        .signal = SIGUSR1,
+        .select_timeout = { .tv_sec = 5, .tv_nsec = 0 }
+    };
+    selector_init(&selector_init);
+
+    fd_selector selector = selector_new(INITIAL_MAX_CLIENTS);
+
+    if (selector == NULL) {
+        perror("Failed to create selector");
+        exit(EXIT_FAILURE);
+    }
+
     //initialise all client_socket[] to 0 so not checked
     for (i = 0; i < max_clients; i++)
     {
         client_socket[i] = 0;
     }
-
-    //create a master socket
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
-    {
-        perror("socket failed");
+    master_socket = setupTCPServerSocket(PORT);
+    if (master_socket < 0) {
+        perror("Failed to setup TCP server socket");
         exit(EXIT_FAILURE);
     }
-
-    //set master socket to allow multiple connections , this is just a good habit, it will work without this
-    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    //type of socket created
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-    //bind the socket to localhost port 8888
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Listener on port %d \n", PORT);
-
-    //try to specify maximum of 3 pending connections for the master socket
-    if (listen(master_socket, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    //accept the incoming connection
-    addrlen = sizeof(address);
-    puts("Waiting for connections ...");
+    selector_register(selector, master_socket, &(fd_handler){
+        .handle_read = NULL, // Define your read handler here
+        .handle_write = NULL, // Define your write handler here
+        .handle_close = NULL, // Define your close handler here
+    }, OP_READ, NULL);
 
     while(TRUE)
     {
-        //clear the socket set
-        FD_ZERO(&readfds);
-
-        //add master socket to set
-        FD_SET(master_socket, &readfds);
-        max_sd = master_socket;
+//        //clear the socket set
+//        FD_ZERO(&readfds);
+//
+//        //add master socket to set
+//        FD_SET(master_socket, &readfds);
+//        max_sd = master_socket;
 
         //add child sockets to set
         for ( i = 0 ; i < max_clients ; i++)
@@ -105,7 +94,7 @@ int main(int argc , char *argv[])
         }
 
         //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+        activity = selector_select( selector);
 
         if ((activity < 0) && (errno!=EINTR))
         {
@@ -115,11 +104,7 @@ int main(int argc , char *argv[])
         //If something happened on the master socket , then its an incoming connection
         if (FD_ISSET(master_socket, &readfds))
         {
-            if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
+
 
             //inform user of socket number - used in send and receive commands
             printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
@@ -161,8 +146,8 @@ int main(int argc , char *argv[])
                     printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
                     //Close the socket and mark as 0 in list for reuse
-                    close( sd );
-                    client_socket[i] = 0;
+                    close( sd ); //se queda
+                    client_socket[i] = 0;//unregister
                 }
 
                 //Echo back the message that came in
